591c191f26db89b47a116d56108a7999e50f7cb8
Andreas Sundqvist <andreas@forme.se>
2019-10-21 20:25:07 +0200

    Cs fix

--- tests/ActionDispatcherTest.php
+++ tests/ActionDispatcherTest.php
@@ -21,254 +21,274 @@ use Psr\Http\Server\RequestHandlerInterface;
 
 class ActionDispatcherTest extends TestCase
 {
-	private function getResolver(callable $responder): ResolverInterface
-	{
-		return new class($responder) implements ResolverInterface {
-			private $responder;
+    private function getResolver(callable $responder): ResolverInterface
+    {
+        return new class($responder) implements ResolverInterface
+        {
+            private $responder;
 
-			public function __construct(callable $responder)
-			{
-				$this->responder = $responder;
-			}
+            public function __construct(callable $responder)
+            {
+                $this->responder = $responder;
+            }
 
-			public function resolveDomain($domain): callable
-			{
-				return $domain;
-			}
+            public function resolveDomain($domain): callable
+            {
+                return $domain;
+            }
 
-			public function resolveInput($input): callable
-			{
-				return $input;
-			}
+            public function resolveInput($input): callable
+            {
+                return $input;
+            }
 
-			public function resolveResponder($responder): callable
-			{
-				return $responder ?: $this->responder;
-			}
-		};
-	}
+            public function resolveResponder($responder): callable
+            {
+                return $responder ?: $this->responder;
+            }
+        };
+    }
 
-	private function getMiddlewareFactory(): MiddlewareFactoryInterface
-	{
-		return new class implements MiddlewareFactoryInterface {
-			public function newInstance(): DispatcherInterface
-			{
-				return new Dispatcher(new Psr17Factory());
-			}
+    private function getMiddlewareFactory(): MiddlewareFactoryInterface
+    {
+        return new class implements MiddlewareFactoryInterface
+        {
+            public function newInstance(): DispatcherInterface
+            {
+                return new Dispatcher(new Psr17Factory());
+            }
 
-			public function newConfiguredInstance(?ContainerInterface $container = null): DispatcherInterface
-			{
-				return new Dispatcher(new Psr17Factory());
-			}
-		};
-	}
+            public function newConfiguredInstance(?ContainerInterface $container = null): DispatcherInterface
+            {
+                return new Dispatcher(new Psr17Factory());
+            }
+        };
+    }
 
-	public function testEmptyAction(): void
-	{
-		$testResponse = new Response();
+    public function testEmptyAction(): void
+    {
+        $testResponse = new Response();
 
-		$dispatcher = new ActionDispatcher(
-			$this->getResolver(function () use ($testResponse) {
-				return $testResponse;
-			}),
-			new Psr17Factory(),
-			$this->getMiddlewareFactory()
-		);
+        $dispatcher = new ActionDispatcher(
+            $this->getResolver(function () use ($testResponse) {
+                return $testResponse;
+            }),
+            new Psr17Factory(),
+            $this->getMiddlewareFactory()
+        );
 
-		$response = $dispatcher->dispatch(
-			new class extends AbstractAction {
-			},
-			new ServerRequest('GET', '/')
-		);
+        $response = $dispatcher->dispatch(
+            new class extends AbstractAction
+            {
+            },
+            new ServerRequest('GET', '/')
+        );
 
-		$this->assertSame($testResponse, $response);
-	}
+        $this->assertSame($testResponse, $response);
+    }
 
-	public function testWithMiddlewares(): void
-	{
-		$testResponse = new Response();
-		$dispatcher = new ActionDispatcher(
-			$this->getResolver(function () use ($testResponse) {
-				return $testResponse;
-			}),
-			new Psr17Factory(),
-			$this->getMiddlewareFactory()
-		);
+    public function testWithMiddlewares(): void
+    {
+        $testResponse = new Response();
+        $dispatcher = new ActionDispatcher(
+            $this->getResolver(function () use ($testResponse) {
+                return $testResponse;
+            }),
+            new Psr17Factory(),
+            $this->getMiddlewareFactory()
+        );
 
-		$response = $dispatcher->dispatch(
-			new class extends AbstractAction {
-				public function getMiddlewares(): array
-				{
-					return [
-						new class implements MiddlewareInterface {
-							public function process(ServerRequestInterface $request, RequestHandlerInterface $handler): ResponseInterface
-							{
-								ActionDispatcherTest::assertTrue(true);
+        $response = $dispatcher->dispatch(
+            new class extends AbstractAction
+            {
+                public function __construct()
+                {
+                    $this->middlewares[] = new class implements MiddlewareInterface
+                    {
+                        public function process(
+                            ServerRequestInterface $request,
+                            RequestHandlerInterface $handler
+                        ): ResponseInterface {
+                            ActionDispatcherTest::assertTrue(true);
 
-								return $handler->handle($request);
-							}
-						}
-					];
-				}
-			},
-			new ServerRequest('GET', '/')
-		);
+                            return $handler->handle($request);
+                        }
+                    };
+                }
+            },
+            new ServerRequest('GET', '/')
+        );
 
-		$this->assertSame($testResponse, $response);
-	}
+        $this->assertSame($testResponse, $response);
+    }
 
-	public function testInputAndDomain(): void
-	{
-		$testResponse = new Response();
-		$dispatcher = new ActionDispatcher(
-			$this->getResolver(static function () {}),
-			new Psr17Factory(),
-			$this->getMiddlewareFactory()
-		);
+    public function testInputAndDomain(): void
+    {
+        $testResponse = new Response();
+        $dispatcher = new ActionDispatcher(
+            $this->getResolver(static function () {
+            }),
+            new Psr17Factory(),
+            $this->getMiddlewareFactory()
+        );
 
-		$testPayload = $this->createMock(Payload::class);
-		$response = $dispatcher->dispatch(
-			new class ($testPayload, $testResponse) extends AbstractAction {
-				private $testPayload;
-				private $testResponse;
+        $testPayload = $this->createMock(Payload::class);
+        $response = $dispatcher->dispatch(
+            new class ($testPayload, $testResponse) extends AbstractAction
+            {
+                private $testPayload;
+                private $testResponse;
 
-				public function __construct($testPayload, $testResponse)
-				{
-					$this->testPayload = $testPayload;
-					$this->testResponse = $testResponse;
-				}
+                public function __construct($testPayload, $testResponse)
+                {
+                    $this->testPayload = $testPayload;
+                    $this->testResponse = $testResponse;
+                }
 
-				public function getInput()
-				{
-					return static function(ServerRequestInterface $request) {
-						return 'testInput';
-					};
-				}
-				public function getDomain()
-				{
-					return function($input) {
-						ActionDispatcherTest::assertSame('testInput', $input);
-						return $this->testPayload;
-					};
-				}
+                public function getInput()
+                {
+                    return static function (ServerRequestInterface $request) {
+                        return 'testInput';
+                    };
+                }
 
-				public function getResponder()
-				{
-					return function ($request, $response, $payload) {
-						ActionDispatcherTest::assertSame($this->testPayload, $payload);
-						return $this->testResponse;
-					};
-				}
-			},
-			new ServerRequest('GET', '/')
-		);
+                public function getDomain()
+                {
+                    return function ($input) {
+                        ActionDispatcherTest::assertSame('testInput', $input);
+                        return $this->testPayload;
+                    };
+                }
 
-		$this->assertSame($testResponse, $response);
-	}
+                public function getResponder()
+                {
+                    return function ($request, $response, $payload) {
+                        ActionDispatcherTest::assertSame($this->testPayload, $payload);
+                        return $this->testResponse;
+                    };
+                }
+            },
+            new ServerRequest('GET', '/')
+        );
 
-	public function testInputException(): void
-	{
-		$testResponse = new Response();
+        $this->assertSame($testResponse, $response);
+    }
 
-		$dispatcher = new ActionDispatcher(
-			$this->getResolver(function () use ($testResponse) {
-				return $testResponse;
-			}),
-			new Psr17Factory(),
-			$this->getMiddlewareFactory()
-		);
+    public function testInputException(): void
+    {
+        $testResponse = new Response();
 
-		$response = $dispatcher->dispatch(
-			new class extends AbstractAction {
-				public function getInput()
-				{
-					return static function () {
-						throw new \DomainException('test');
-					};
-				}
-				public function getDomain()
-				{
-					return static function () {};
-				}
-			},
-			new ServerRequest('GET', '/')
-		);
+        $dispatcher = new ActionDispatcher(
+            $this->getResolver(function () use ($testResponse) {
+                return $testResponse;
+            }),
+            new Psr17Factory(),
+            $this->getMiddlewareFactory()
+        );
 
-		$this->assertSame($testResponse, $response);
-	}
+        $response = $dispatcher->dispatch(
+            new class extends AbstractAction
+            {
+                public function getInput()
+                {
+                    return static function () {
+                        throw new \DomainException('test');
+                    };
+                }
 
-	public function testNextAction(): void
-	{
-		$testResponse = new Response();
+                public function getDomain()
+                {
+                    return static function () {
+                    };
+                }
+            },
+            new ServerRequest('GET', '/')
+        );
 
-		$dispatcher = new ActionDispatcher(
-			$this->getResolver(function () use ($testResponse) {
-				return $testResponse;
-			}),
-			new Psr17Factory(),
-			$this->getMiddlewareFactory()
-		);
+        $this->assertSame($testResponse, $response);
+    }
 
-		$response = $dispatcher->dispatch(
-			new class extends AbstractAction {
-				public function getInput()
-				{
-					return static function(ServerRequestInterface $request) {
-						ActionDispatcherTest::assertFalse($request->getAttribute('currentResponse', false));
+    public function testNextAction(): void
+    {
+        $testResponse = new Response();
 
-						return 'test1';
-					};
-				}
-				public function getDomain()
-				{
-					return static function () {};
-				}
-				public function getNextAction()
-				{
-					return new class extends AbstractAction {
-						public function getInput()
-						{
-							return static function(ServerRequestInterface $request) {
-								ActionDispatcherTest::assertInstanceOf(ResponseInterface::class, $request->getAttribute('currentResponse'));
+        $dispatcher = new ActionDispatcher(
+            $this->getResolver(function () use ($testResponse) {
+                return $testResponse;
+            }),
+            new Psr17Factory(),
+            $this->getMiddlewareFactory()
+        );
 
-								return 'test2';
-							};
-						}
-						public function getDomain()
-						{
-							return static function () {};
-						}
-					};
-				}
-			},
-			new ServerRequest('GET', '/')
-		);
+        $response = $dispatcher->dispatch(
+            new class extends AbstractAction
+            {
+                public function getInput()
+                {
+                    return static function (ServerRequestInterface $request) {
+                        ActionDispatcherTest::assertFalse($request->getAttribute('currentResponse', false));
 
-		$this->assertSame($testResponse, $response);
-	}
+                        return 'test1';
+                    };
+                }
 
-	public function testInvalidResponder(): void
-	{
-		$testResponse = new Response();
+                public function getDomain()
+                {
+                    return static function () {
+                    };
+                }
 
-		$dispatcher = new ActionDispatcher(
-			$this->getResolver(function () use ($testResponse) {
-				return $testResponse;
-			}),
-			new Psr17Factory(),
-			$this->getMiddlewareFactory()
-		);
+                public function getNextAction()
+                {
+                    return new class extends AbstractAction
+                    {
+                        public function getInput()
+                        {
+                            return static function (ServerRequestInterface $request) {
+                                ActionDispatcherTest::assertInstanceOf(ResponseInterface::class,
+                                    $request->getAttribute('currentResponse'));
 
-		$response = $dispatcher->dispatch(
-			new class extends AbstractAction {
-				public function getResponder()
-				{
-					return 'error';
-				}
-			},
-			new ServerRequest('GET', '/')
-		);
+                                return 'test2';
+                            };
+                        }
 
-		$this->assertSame($testResponse, $response);
-	}
+                        public function getDomain()
+                        {
+                            return static function () {
+                            };
+                        }
+                    };
+                }
+            },
+            new ServerRequest('GET', '/')
+        );
+
+        $this->assertSame($testResponse, $response);
+    }
+
+    public function testInvalidResponder(): void
+    {
+        $testResponse = new Response();
+
+        $dispatcher = new ActionDispatcher(
+            $this->getResolver(function () use ($testResponse) {
+                return $testResponse;
+            }),
+            new Psr17Factory(),
+            $this->getMiddlewareFactory()
+        );
+
+        $this->expectException(\TypeError::class);
+
+        $response = $dispatcher->dispatch(
+            new class extends AbstractAction
+            {
+                public function getResponder()
+                {
+                    return 'error';
+                }
+            },
+            new ServerRequest('GET', '/')
+        );
+    }
 }
